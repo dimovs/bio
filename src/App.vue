f24e0181389b25d63ae10988d20b16d18ff65743
Dmitriy Ovsyannikov <dnovsyannikov@sberbank.ru>
2021-12-06 18:07:12 +0500

    create personal website

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
 <style>
+*,
+::after,
+::before {
+  box-sizing: border-box;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+h1, h2, p {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+html,
+body,
 #app {
+  height: 100%;
+}
+
+html {
+  background-color: whitesmoke;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+body {
+  margin: 0;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+a {
+ color: inherit;
+  text-decoration: none;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+img {
+  max-width: 100%;
 }
 </style>
---
+++ src/components/Friends.vue
@@ -0,0 +1,80 @@
+<template>
+  <h2  class="header">Моя друзья</h2>
+  <p class="text">
+    У меня много друзей. Мы вместе проводим все свободное время за рукоделием.
+  </p>
+  <ul class="images">
+    <li class="images__item">
+      <a href="/img/friends2.8393ef6c.jpg" target="_blank">
+        Это мы на вечеринке
+        <img src="../assets/friends2.jpg" alt="Мои друзья">
+      </a>
+    </li>
+    <li class="images__item">
+      <a href="/img/friends2.8393ef6c.jpg" target="_blank">
+        Это мы в походе
+      <img src="../assets/friends2.jpg" alt="Мои друзья">
+      </a>
+    </li>
+    <li class="images__item">
+      <a href="/img/friends2.8393ef6c.jpg" target="_blank">
+        Это мы на вечеринке
+      <img src="../assets/friends2.jpg" alt="Мои друзья">
+      </a>
+    </li>
+    <li class="images__item">
+      <a href="/img/friends2.8393ef6c.jpg" target="_blank">
+        Это мы в походе
+      <img src="../assets/friends2.jpg" alt="Мои друзья">
+      </a>
+    </li>
+    <li class="images__item">
+      <a href="/img/friends2.8393ef6c.jpg" target="_blank">
+        Это мы на вечеринке
+      <img src="../assets/friends2.jpg" alt="Мои друзья">
+      </a>
+    </li>
+    <li class="images__item">
+      <a href="/img/friends2.8393ef6c.jpg" target="_blank">
+        Это мы на вечеринке
+      <img src="../assets/friends2.jpg" alt="Мои друзья">
+      </a>
+    </li>
+    <li class="images__item">
+      <a href="/img/friends2.8393ef6c.jpg" target="_blank">
+        Это мы на вечеринке
+      <img src="../assets/friends2.jpg" alt="Мои друзья">
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Friends'
+}
+</script>
+
+<style scoped>
+.header {
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.text {
+  margin-bottom: 30px;
+}
+
+.images {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  gap: 20px 20px;
+}
+
+.images__item {
+  text-align: center;
+  width: 250px;
+}
+</style>
